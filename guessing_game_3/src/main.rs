1cdcb2e186f626eaaa8b6f6d511c1d1d97527af7
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2025-01-16 17:57:14 -0500

    terminar projecto 3 question_game

--- guessing_game_3/src/main.rs
+++ guessing_game_3/src/main.rs
@@ -1,36 +1,51 @@
 extern crate rand;
 
-use std::io;
 use rand::Rng; //trait
-use std::cmp::Ordering; //enumerador
+use std::cmp::Ordering;
+use std::io; //enumerador
 
 fn main() {
     println!("!Adivina el numero¡");
 
     let secret_number = rand::thread_rng()
-        .gen_range(1.. 101);
+        .gen_range(1..101);
 
-    println!("El numero secreto es: {}", secret_number);
+    loop {
+        println!("El numero secreto es: {}", secret_number);
 
-    println!("Introdusca su adivinanza.");
+        println!("Introdusca su adivinanza.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("fallo al leer la linea");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("fallo al leer la linea");
 
-    let guess: u32 = guess
-        .trim()
-        .parse()
-        .expect("escriba un numero");
+        let guess: u32 = match guess.trim().parse() { //recordar que parce retorna un result tipo enum con variante ok y error, con match hacemos "como switch"
+                Ok(num) => num,
+                Err(_) => continue
+        };
 
-    let comparison_result = guess.cmp(&secret_number); //comparamos quess y el secret_number
+        let comparison_result = guess
+            .cmp(&secret_number); //comparamos quess y el secret_number
 
-    match comparison_result {
-        Ordering::Less => println!("numero pequeño"),
-        Ordering::Greater => println!("numero grande"),
-        Ordering::Equal => println!("numero igual")
-    };
+        match comparison_result {
+            Ordering::Less => println!("numero pequeño"),
+            Ordering::Greater => println!("numero grande"),
+            Ordering::Equal => {
+                println!("numero igual");
+                break;
+            }
+        };
 
-    println!("Tu numero: {}", guess);
+        println!("Tu numero: {}", guess);
+    }
 }
+
+
+//recordar lo siquiente 
+//  guess.trim().parse() retorna un result que es un tipo enum y que tiene el valor ok y error
+//  match value {} es como si fuera un switch
+//  entonces:
+//  en una sola line ponesmo poner "let guess: u32 = match guess.trim().parse() { ok... err... }" recordar que match es el que tiene los {} no parse()
+
