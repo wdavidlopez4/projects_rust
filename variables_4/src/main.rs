7aa225fa6a006265f82314184a9db8a7fe34aca4
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2025-01-23 19:44:02 -0500

    projecto variables tipos de datos

--- variables_4/src/main.rs
+++ variables_4/src/main.rs
@@ -34,4 +34,27 @@ fn main() {
     let spaces = spaces.len();
     println!("variable sombreada que cambia de tipo de valor: {}", spaces);
 
+
+    //ejemplo variable entera
+    let con_signo : u32 = 12121;
+    let sin_signo : i32 =  -112;
+
+    println!("con signo: {}, sin signo: {}", con_signo, sin_signo);
+
+    //ejemplo variable decimal
+    let coma_flotante : f32 = 32.122;
+    println!("coma flotante: {}", coma_flotante);
+
+    //ejemplo de operacion aritmetica
+    let resto = 43 % 5;
+    let divicion = 43 /5;
+
+    println!("divicion: {}, resto: {}", divicion, resto);
+
+    //ejemplo variable boleana
+    let variable_boleana = true;
+    let variable_true: bool = true;
+
+    println!("variable bolena: {}, notacion explixita: {}", variable_boleana, variable_true);
+
 }
