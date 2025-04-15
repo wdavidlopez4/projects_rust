5e2e46aced9fd9619bd09fd197c3fe96c722e6d1
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2025-04-15 17:37:16 -0500

    segundo ejemplo

--- rectangless_20/src/main.rs
+++ rectangless_20/src/main.rs
@@ -1,15 +1,31 @@
 
-/*
-    primera version de programa para calcular rectangulo
-    sin utilizar estructuras
- */
+
 fn main() {
+
+    /*
+        primera version de programa para calcular rectangulo
+        sin utilizar estructuras
+        no se relacionan las variables
+    */
     let width1 = 30;
     let height1 = 50;
 
     println!("el area es {} en pixeles", area(width1, height1)); 
+
+    /*
+        segunda version de programa para calcular rectangulo
+        utilizando tuplas
+        el problema de esta version es que nos podemos confundir cual es el ancho o el alto
+    */
+    let rect1 = (30, 50);
+    println!("el area es {} en pixeles con tupla", area_tuplas(rect1));
+
 }
 
 fn area(width: u32, height: u32) -> u32 {
     width * height
+}
+
+fn area_tuplas(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
