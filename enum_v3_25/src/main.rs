ea885e4f9591afb9f1cd2d2ee66cd90e0f0df699
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2025-04-30 15:16:17 -0500

    v3 elementos de enums

--- enum_v3_25/src/main.rs
+++ enum_v3_25/src/main.rs
@@ -1,3 +1,8 @@
+
+/*
+ejemplo de estructuras dentro de un enums
+ */
+
 #[derive(Debug)]
 struct V4Addr{
     name: String
@@ -14,8 +19,28 @@ enum IPAddr{
     V6(V6Addr)
 }
 
+/*
+ejemplo de diferentes elementos dentro de un enums,
+y los enums al igual que las estructuras pueden tener metodos
+ */
+
+ enum Message{
+    Quit, //no tiene ningun dato asociado solo es un tipo
+    Move {x : i32, y: i32}, //struct
+    Write(String), //tiene asociado un string
+    ChangeColor(i32, i32, i32) //asociada una tupla
+ }
+
+ impl Message{
+    fn Call(&self){
+        //mas adelante miramos como implementar el curpo
+    }
+ }
+
 fn main() {
     let v4 = IPAddr::V4(V4Addr {name:  String::from("123.12.13.0")});
-
     println!("{:?}", v4);
+
+    let ex = Message::Quit;
+    ex.Call();
 }
