e693c09b5c3aa1760fb84a62f065959682158231
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2025-06-18 18:41:12 -0500

    como renombrar

--- module_use_41/src/main.rs
+++ module_use_41/src/main.rs
@@ -15,8 +15,22 @@ mod instruments{
 
 use crate::instruments::guitar; //absolute path
 use self::instruments::piano; //relative path (puede que cambie en el futoru es una inconsistencia de rust poner self)
+use std::fmt::Result;
+use std::io::Result as IoResult; //renombrar Result a IoResult
 
 fn main() {
     guitar::play();
     piano::play();
+
+    _ =  example_io_result();
+    _ = example_result();
+}
+
+// Ejemplo de como se llama a un elemento renombrado
+fn example_io_result() -> IoResult<()>{
+    Ok(())
+}
+
+fn example_result() -> Result{
+    Ok(())
 }
