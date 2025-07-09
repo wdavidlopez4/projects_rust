ec1522e8d92acb06c322acba890e2b1563ada3e4
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2025-07-09 18:27:24 -0500

    iteradores y enumeradores para los vectores

--- collections_43/src/main.rs
+++ collections_43/src/main.rs
@@ -30,4 +30,36 @@ fn main() {
         Some(value) => println!("El tercer elemento es: {}", value),
         None => println!("No hay un tercer elemento."),
     }
+
+    //recorriendo un vector
+    let vector1 = vec![1, 2, 3, 4, 5 ];
+    for i in &vector1{
+        println!("El elemento es: {}", i);
+    }
+
+    //recorriendo un vector mutable
+    let mut vector2 = vec![1, 2, 3, 4];
+    for v in &mut vector2{
+        *v += 50; // Modificando el valor del elemento (el * es para desreferenciar la variable mutable)
+        println!("El elemento modificado es: {}", v);
+    }
+
+    println!("Vector modificado: {:?}", vector2);
+
+
+    //ejemplo de vector con enum para utilizar diferentes tipos de datos
+    #[derive(Debug)]
+    enum SheetCell{
+        Int(i32),
+        Float(f64),
+        Text(String)
+    }
+
+    let row = vec![
+        SheetCell::Int(3),
+        SheetCell::Float(3.14),
+        SheetCell::Text(String::from("12"))
+    ];
+
+    println!("Fila: {:?}", row[0]);
 }
