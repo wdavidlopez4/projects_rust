bd89b52814e4debf287c51fda07cea4c06bc0911
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2025-08-23 18:49:11 -0500

    parametros trait y sobreescribir

--- trait_44/src/main.rs
+++ trait_44/src/main.rs
@@ -2,6 +2,14 @@
 //trait
 pub trait Summary{
     fn summarize(&self) -> String;
+
+    fn sumarize_author(&self) -> String{
+        String::from("more...")
+    }
+
+    fn sumarize_location(&self) -> &str{
+        "more..."
+    }
 }
 
 //struct
@@ -19,11 +27,20 @@ impl NewArtice { //impl sin trait sus propios metodos
             autor: String::from("william"),
         }
     }
+
+    pub fn noty(item: &impl Summary) -> String { //resibe por parametro un trait tipo summary
+        let sumarize = item.summarize();
+        format!("execute sumarize : {} ", sumarize)
+    }
 }
 
 impl Summary for NewArtice { //impl implementando trait
-    fn summarize(&self) -> String {
+    fn summarize(&self) -> String { //implementar el metodo
         format!("{}, {} de {}", self.headline, self.location, self.autor)
+    }
+
+    fn sumarize_location(&self) -> &str { //sobrescribir el metodo
+        &self.location
     }
 }
 
@@ -35,5 +52,11 @@ fn main() {
         String::from("Colombia"),
     );
 
-    println!("{}", s.summarize());
+    let noty = NewArtice::noty(&s);
+
+    println!("{} - {} - {} - {}", 
+        s.summarize(), 
+        s.sumarize_author(), 
+        s.sumarize_location(),
+        noty);
 }
