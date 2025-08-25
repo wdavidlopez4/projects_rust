fb8ab2d7ef8767e0a5a84c7aeb210424557bedae
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2025-08-25 10:26:53 -0500

    generico imp

--- exercises_44/exercises_45/src/main.rs
+++ exercises_44/exercises_45/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 fn main() {
     //utilizamos numeros
     let number_list = vec![34, 50, 25, 100, 65];
@@ -9,8 +11,16 @@ fn main() {
     let result = largest(&char_list);
     println!("The largest char is {}", result);
 
+
+    //utilizando el 2 ejemplo
+    let p = Pair::new(2, 4);
+    p.cmp_display();
+
 }
 
+/*
+ejemplo 1 metodo generico con clausula where
+ */
 fn largest<T>(list: &[T]) -> T 
     where T: PartialOrd + Copy
 {
@@ -24,3 +34,29 @@ fn largest<T>(list: &[T]) -> T
 
     largest
 }
+
+/*
+implementacion generica
+ */
+
+ struct Pair<T>{
+    x: T,
+    y: T
+ }
+
+ impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self{
+        Self{x, y}
+    }
+ }
+
+ impl <T: Display + PartialOrd> Pair<T> { //Solo resibe de tipos Display y partiaord tambien se puede utilizar el where
+     fn cmp_display(&self){
+        if self.x >= self.y{
+            println!("The lasgert menber is x = {}", self.x)
+        }
+        else {
+            println!("The lasgert menber is y = {}", self.y)
+        }
+     }
+ }
